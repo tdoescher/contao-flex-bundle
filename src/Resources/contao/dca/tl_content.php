2df861d81c4d11d22d9f8dd2db2d3128f62ecf50
Torben Döscher <mail@tdoescher.de>
2023-01-23 19:17:36 +0000

    Update

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,127 +1,120 @@
 <?php
 
-/**
- * This file is part of FlexBundle for Contao
- *
- * @package     tdoescher/flex-bundle
- * @author      Torben Döscher <mail@tdoescher.de>
- * @license     LGPL
- * @copyright   tdoescher.de // WEB & IT <https://tdoescher.de>
- */
-
-$GLOBALS['TL_DCA']['tl_content']['palettes']['flex_open'] = '{type_legend},type;{flex_legend},flex_xs,flex_sm,flex_md,flex_lg,flex_xl,flex_xxl;{flex_extended_legend},flex_class,flex_repeat,flex_bootstrap,flex_multiline,flex_justify,flex_align,flex_gutter,flex_container_class;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['flex_open'] = '{type_legend},type;{flex_legend},flex_xs,flex_sm,flex_md,flex_lg,flex_xl,flex_xxl;{flex_extended_legend},flex_class,flex_repeat,flex_bootstrap,flex_multiline,flex_justify,flex_align,flex_container_class;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['flex_close'] = '{type_legend},type;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['flex_div_open'] = '{type_legend},type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['flex_div_close'] = '{type_legend},type;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_xs'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_xs'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_xs'],
-    'exclude'   => true,
+    'exclude'    => true,
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'flex', 'tl_class' => 'w50', 'maxlength' => '255'],
+    'eval'      => array('rgxp' => 'flex', 'tl_class' => 'w50', 'maxlength' => '255'),
     'sql'       => "varchar(255) NOT NULL default ''"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_sm'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_sm'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_sm'],
     'exclude'   => true,
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'flex', 'tl_class' => 'w50', 'maxlength' => '255'],
+    'eval'      => array('rgxp' => 'flex', 'tl_class' => 'w50', 'maxlength' => '255'),
     'sql'       => "varchar(255) NOT NULL default ''"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_md'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_md'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_md'],
     'exclude'   => true,
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'flex', 'tl_class' => 'clr w50', 'maxlength' => '255'],
+    'eval'      => array('rgxp' => 'flex', 'tl_class' => 'clr w50', 'maxlength' => '255'),
     'sql'       => "varchar(255) NOT NULL default ''"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_lg'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_lg'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_lg'],
     'exclude'   => true,
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'flex', 'tl_class' => 'w50', 'maxlength' => '255'],
+    'eval'      => array('rgxp' => 'flex', 'tl_class' => 'w50', 'maxlength' => '255'),
     'sql'       => "varchar(255) NOT NULL default ''"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_xl'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_xl'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_xl'],
     'exclude'   => true,
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'flex', 'tl_class' => 'clr w50', 'maxlength' => '255'],
+    'eval'      => array('rgxp' => 'flex', 'tl_class' => 'clr w50', 'maxlength' => '255'),
     'sql'       => "varchar(255) NOT NULL default ''"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_xxl'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_xxl'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_xxl'],
     'exclude'   => true,
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'flex', 'tl_class' => 'w50', 'maxlength' => '255'],
+    'eval'      => array('rgxp' => 'flex', 'tl_class' => 'w50', 'maxlength' => '255'),
     'sql'       => "varchar(255) NOT NULL default ''"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_class'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_class'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_class'],
     'exclude'   => true,
     'inputType' => 'text',
-    'eval'      => ['rgxp' => 'flex', 'maxlength' => '255'],
+    'eval'      => array('rgxp' => 'flex', 'maxlength' => '255'),
     'sql'       => "varchar(255) NOT NULL default ''"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_repeat'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_repeat'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_repeat'],
     'exclude'   => true,
     'default'   => 1,
     'inputType' => 'checkbox',
-    'eval'      => ['tl_class' => 'clr w50'],
+    'eval'      => array('tl_class' => 'clr w50'),
     'sql'       => "char(1) NOT NULL default '1'"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_bootstrap'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_bootstrap'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_bootstrap'],
     'exclude'   => true,
     'default'   => 1,
     'inputType' => 'checkbox',
-    'eval'      => ['tl_class' => 'w50'],
+    'eval'      => array('tl_class' => 'w50'),
     'sql'       => "char(1) NOT NULL default '1'"
-];
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_justify'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_justify'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_justify'],
     'exclude'   => true,
     'inputType' => 'select',
-    'options'   => ['normal', 'start', 'end', 'center', 'around', 'between', 'evenly'],
+    'options'   => array('normal', 'start', 'end', 'center', 'around', 'between'),
     'reference' => &$GLOBALS['TL_LANG']['tl_content']['flex_justify_options'],
-    'eval'      => ['helpwizard' => false, 'chosen' => false, 'tl_class' => 'w50'],
-    'sql'       => "varchar(7) NOT NULL default 'normal'"
-];
+    'eval'      => array('helpwizard' => false, 'chosen' => false, 'tl_class' => 'w50'),
+    'sql'       => array('name' => 'flex_justify', 'type' => 'string', 'length' => 13, 'default' => 'normal')
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_align'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_align'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_align'],
     'exclude'   => true,
     'inputType' => 'select',
-    'options'   => ['normal', 'start', 'end', 'center', 'baseline', 'stretch'],
+    'options'   => array('normal', 'start', 'end', 'center', 'baseline', 'stretch'),
     'reference' => &$GLOBALS['TL_LANG']['tl_content']['flex_align_options'],
-    'eval'      => ['helpwizard' => false, 'chosen' => false, 'tl_class' => 'w50'],
-    'sql'       => "varchar(8) NOT NULL default 'normal'"
-];
+    'eval'      => array('helpwizard' => false, 'chosen' => false, 'tl_class' => 'w50'),
+    'sql'       => array('name' => 'flex_align', 'type' => 'string', 'length' => 13, 'default' => 'normal')
+);
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_gutter'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_gutter'],
-    'exclude'   => true,
-    'inputType' => 'select',
-    'options'   => ['0', '1', '2', '3', '4', '5'],
-    'reference' => &$GLOBALS['TL_LANG']['tl_content']['flex_gutter_options'],
-    'eval'      => ['helpwizard' => false, 'chosen' => false, 'tl_class' => 'clr w50'],
-    'sql'       => "varchar(1) NOT NULL default '4'"
-];
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_container_class'] = [
+$GLOBALS['TL_DCA']['tl_content']['fields']['flex_container_class'] = array
+(
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_container_class'],
     'exclude'   => true,
     'inputType' => 'text',
-    'eval'      => ['maxlength' => '255', 'tl_class' => 'w50'],
+    'eval'      => array('maxlength' => '255', 'tl_class' => 'clr'),
     'sql'       => "varchar(255) NOT NULL default ''"
-];
+);
--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of FlexBundle for Contao
- *
- * @package     tdoescher/flex-bundle
- * @author      Torben Döscher <mail@tdoescher.de>
- * @license     LGPL
- * @copyright   tdoescher.de // WEB & IT <https://tdoescher.de>
- */
-
 $GLOBALS['TL_LANG']['tl_content']['flex_legend'] = 'Flex';
 $GLOBALS['TL_LANG']['tl_content']['flex_repeat'] = ['Wiederholen', 'Soll die Aufteilung wiederholt werden?'];
 $GLOBALS['TL_LANG']['tl_content']['flex_bootstrap'] = ['Bootstrap', 'Sollen die Bootstrap Klassen gesetzt werden?'];
@@ -19,12 +10,9 @@ $GLOBALS['TL_LANG']['tl_content']['flex_md'] = ['Tablet (md, 768px)', 'Bsp.: "3:
 $GLOBALS['TL_LANG']['tl_content']['flex_lg'] = ['Desktop (lg, 992px)', 'Bsp.: "2:4:4:2" für vier Spalten 16,6% / 33,3% / 33,3% / 16,6%'];
 $GLOBALS['TL_LANG']['tl_content']['flex_xl'] = ['Widescreen (xl, 1200px)', 'Bsp.: "4" für drei gleich große Spalten '];
 $GLOBALS['TL_LANG']['tl_content']['flex_xxl'] = ['Widescreen XXL (xxl, 1400px)', 'Bsp.: "6" für zwölf gleich große Spalten '];
-$GLOBALS['TL_LANG']['tl_content']['flex_class'] = ['Spalten Klassen', 'Zuweisung von CSS-Klassen zu den Spalten.'];
+$GLOBALS['TL_LANG']['tl_content']['flex_class'] = ['Klassen Spalten', 'Zuweisung von CSS-Klassen zu den Spalten.'];
 $GLOBALS['TL_LANG']['tl_content']['flex_justify'] = ['Ausrichtung horizontal', 'Wie sollen die Elemente horizontal ausgerichtet werden?'];
-$GLOBALS['TL_LANG']['tl_content']['flex_justify_options'] = ['normal' => '-', 'start' => 'flex-start', 'end' => 'flex-end', 'center' => 'center', 'around' => 'space-around', 'between' => 'space-between', 'evenly' => 'space-evenly'];
+$GLOBALS['TL_LANG']['tl_content']['flex_justify_options'] = ['normal' => '-', 'start' => 'flex-start', 'end' => 'flex-end', 'center' => 'center', 'around' => 'space-around', 'between' => 'space-between'];
 $GLOBALS['TL_LANG']['tl_content']['flex_align'] = ['Ausrichtung vertikal', 'Wie sollen die Elemente vertikal ausgerichtet werden?'];
 $GLOBALS['TL_LANG']['tl_content']['flex_align_options'] = ['normal' => '-', 'start' => 'flex-start', 'end' => 'flex-end', 'center' => 'center', 'baseline' => 'baseline', 'stretch' => 'stretch'];
-$GLOBALS['TL_LANG']['tl_content']['flex_gutter'] = ['Spaltenabstand', 'Abstand zwischen den spalten. Standard: 1.5rem'];
-$GLOBALS['TL_LANG']['tl_content']['flex_gutter_options'] = ['0' => '0', '1' => '0.25rem', '2' => '0.5rem', '3' => '1rem', '4' => '1.5rem', '5' => '3rem'];
-$GLOBALS['TL_LANG']['tl_content']['flex_container_class'] = ['Container Klassen', 'Zuweisung von CSS-Klassen für den Container.'];
-$GLOBALS['TL_LANG']['tl_content']['flex_main_class'] = 'Klassen';
+$GLOBALS['TL_LANG']['tl_content']['flex_container_class'] = ['Klassen Container', 'Zuweisung von CSS-Klassen für den Container.'];
