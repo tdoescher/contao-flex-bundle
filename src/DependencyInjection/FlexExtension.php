68dc5297d9fc390c591bbdbb8ed84bf734f32e7c
Torben Döscher <mail@tdoescher.de>
2023-02-09 17:25:11 +0000

    Update

--- src/DependencyInjection/FlexExtension.php
+++ src/DependencyInjection/FlexExtension.php
@@ -18,10 +18,10 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class FlexExtension extends Extension
 { 
-    public function load(array $mergedConfig, ContainerBuilder $container)
-    {
-        $loader = new YamlFileLoader($container, new FileLocator( __DIR__ . '/../Resources/config'));
-        $loader->load('controller.yaml');
-        $loader->load('listener.yaml');
-    }
+  public function load(array $mergedConfig, ContainerBuilder $container)
+  {
+    $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    $loader->load('controller.yaml');
+    $loader->load('listener.yaml');
+  }
 }
