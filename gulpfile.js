2950df276aa7847a02a8279925f3dc99398aaea1
Torben Döscher <mail@tdoescher.de>
2025-04-16 11:02:31 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ import esbuild from 'gulp-esbuild';
 import { sassPlugin } from 'esbuild-sass-plugin';
 
 const styles = () => {
-    return gulp.src([ 'src/Resources/public/grid.scss' ])
+    return gulp.src([ 'src/Resources/public/flexbox.scss' ])
         .pipe(esbuild({
             target: [ 'es2020', 'chrome58', 'edge16', 'firefox57', 'node12', 'safari11' ],
             plugins: [ sassPlugin() ],
             minify: true,
-            outfile: 'grid.min.css',
+            outfile: 'flexbox.min.css',
         }))
         .pipe(gulp.dest('src/Resources/public/'));
 };
---
+++ src/Resources/contao/dca/tl_layout.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['framework']['options'][] = 'flexbox.css';
