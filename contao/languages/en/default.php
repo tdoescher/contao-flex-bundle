65a046964f43a62a7aa547cac0c3d4165165be02
Torben Döscher <mail@tdoescher.de>
2026-02-26 15:39:45 +0100

    English language added & Code review

---
+++ contao/languages/en/default.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This file is part of FlexBundle for Contao
+ *
+ * @package     tdoescher/flex-bundle
+ * @author      Torben Döscher <mail@tdoescher.de>
+ * @license     LGPL
+ * @copyright   tdoescher.de // WEB & IT <https://tdoescher.de>
+ */
+
+$GLOBALS['TL_LANG']['CTE']['flex'] = ['Flex', 'Creates a flex/grid layout.'];
---
+++ contao/languages/en/tl_content.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of FlexBundle for Contao
+ *
+ * @package     tdoescher/flex-bundle
+ * @author      Torben Döscher <mail@tdoescher.de>
+ * @license     LGPL
+ * @copyright   tdoescher.de // WEB & IT <https://tdoescher.de>
+ */
+
+$GLOBALS['TL_LANG']['tl_content']['flex_legend'] = 'Flex';
+$GLOBALS['TL_LANG']['tl_content']['flex_repeat'] = ['Repeat', 'Should the layout pattern be repeated?'];
+$GLOBALS['TL_LANG']['tl_content']['flex_bootstrap'] = ['Framework', 'Which CSS framework/classes should be applied?'];
+$GLOBALS['TL_LANG']['tl_content']['flex_bootstrap_options'] = ['0' => 'none', '1' => 'Flexbox (Bootstrap)', '2' => 'Grid'];
+$GLOBALS['TL_LANG']['tl_content']['flex_extended_legend'] = 'Flex extended';
+$GLOBALS['TL_LANG']['tl_content']['flex_xs'] = ['Smaller than smartphone (xs, less than 576px)', 'E.g.: "12" for one column at 100%'];
+$GLOBALS['TL_LANG']['tl_content']['flex_sm'] = ['Smartphone or larger (sm, 576px)', 'E.g.: "3:9" for two columns 25% / 75%'];
+$GLOBALS['TL_LANG']['tl_content']['flex_md'] = ['Tablet (md, 768px)', 'E.g.: "3:3:6" for three columns 25% / 25% / 50%'];
+$GLOBALS['TL_LANG']['tl_content']['flex_lg'] = ['Desktop (lg, 992px)', 'E.g.: "2:4:4:2" for four columns 16.6% / 33.3% / 33.3% / 16.6%'];
+$GLOBALS['TL_LANG']['tl_content']['flex_xl'] = ['Widescreen (xl, 1200px)', 'E.g.: "4" for three equally sized columns'];
+$GLOBALS['TL_LANG']['tl_content']['flex_xxl'] = ['Widescreen XXL (xxl, 1400px)', 'E.g.: "6" for twelve equally sized columns'];
+$GLOBALS['TL_LANG']['tl_content']['flex_class'] = ['Column classes', 'Assign CSS classes to the columns.'];
+$GLOBALS['TL_LANG']['tl_content']['flex_justify'] = ['Horizontal alignment', 'How should the elements be aligned horizontally?'];
+$GLOBALS['TL_LANG']['tl_content']['flex_justify_options'] = ['normal' => '-', 'start' => 'flex-start', 'end' => 'flex-end', 'center' => 'center', 'around' => 'space-around', 'between' => 'space-between', 'evenly' => 'space-evenly'];
+$GLOBALS['TL_LANG']['tl_content']['flex_align'] = ['Vertical alignment', 'How should the elements be aligned vertically?'];
+$GLOBALS['TL_LANG']['tl_content']['flex_align_options'] = ['normal' => '-', 'start' => 'flex-start', 'end' => 'flex-end', 'center' => 'center', 'baseline' => 'baseline', 'stretch' => 'stretch'];
+$GLOBALS['TL_LANG']['tl_content']['flex_gutter'] = ['Column gap', 'Gap between columns. Default: 1.5rem'];
+$GLOBALS['TL_LANG']['tl_content']['flex_gutter_y'] = ['Row gap', 'Gap between rows.'];
+$GLOBALS['TL_LANG']['tl_content']['flex_gutter_options'] = ['d' => '-', '0' => '0', '1' => '0.25rem', '2' => '0.5rem', '3' => '1rem', '4' => '1.5rem', '5' => '3rem'];
+$GLOBALS['TL_LANG']['tl_content']['flex_container_class'] = ['Container classes', 'Assign CSS classes to the container.'];
+$GLOBALS['TL_LANG']['tl_content']['flex_main_class'] = 'Classes';
